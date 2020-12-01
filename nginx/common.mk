e76780232f85d5b7f49daa434b8ae0a1d697ae90
NaughtyGitHub <37618990+NaughtyGitHub@users.noreply.github.com>
2020-12-01 22:06:25 +0800

    add

---
+++ nginx/common.mk
@@ -0,0 +1,109 @@
+﻿
+#.PHONY:all clean 
+
+ifeq ($(DEBUG),true)
+#-g是生成调试信息。GNU调试器可以利用该信息
+CC = g++ -std=c++11 -g 
+VERSION = debug
+else
+CC = g++ -std=c++11
+VERSION = release
+endif
+
+#CC = gcc
+
+# $(wildcard *.c)表示扫描当前目录下所有.c文件
+#SRCS = nginx.c ngx_conf.c
+SRCS = $(wildcard *.cxx)
+OBJS = $(SRCS:.cxx=.o)
+#把字符串中的.c替换为.d
+#DEPS = nginx.d ngx_conf.d
+DEPS = $(SRCS:.cxx=.d)
+
+#可以指定BIN文件的位置,addprefix是增加前缀函数
+#BIN = /mnt/hgfs/linux/nginx
+BIN := $(addprefix $(BUILD_ROOT)/,$(BIN))
+
+#注意下边这个字符串，末尾不要有空格等否则会语法错误
+LINK_OBJ_DIR = $(BUILD_ROOT)/app/link_obj
+DEP_DIR = $(BUILD_ROOT)/app/dep
+
+#-p是递归创建目录，没有就创建，有就不需要创建了
+$(shell mkdir -p $(LINK_OBJ_DIR))
+$(shell mkdir -p $(DEP_DIR))
+
+#处理后形如 /mnt/hgfs/linux/nginx/app/link_obj/ngx_signal2.o /mnt/hgfs/linux/nginx/app/link_obj/ngx_signal.o
+# := 在解析阶段直接赋值常量字符串【立即展开】，而 = 在运行阶段，实际使用变量时再进行求值【延迟展开】
+# /mnt/hgfs/linux/nginx/app/link_obj/nginx.o   /mnt/hgfs/linux/nginx/app/link_obj/ngx_conf.o 
+OBJS := $(addprefix $(LINK_OBJ_DIR)/,$(OBJS))
+DEPS := $(addprefix $(DEP_DIR)/,$(DEPS))
+
+#找到目录中的所有.o文件（编译出来的）
+LINK_OBJ = $(wildcard $(LINK_OBJ_DIR)/*.o)
+LINK_OBJ += $(OBJS)
+
+#-------------------------------------------------------------------------------------------------------
+#目标：目标依赖【可以省略】
+#	要执行的命令【可以省略】
+#如下这行会是开始执行的入口，执行就找到依赖项$(BIN)去执行了，同时，这里也依赖了$(DEPS)，这样就会生成很多.d文件了
+all:$(DEPS) $(OBJS) $(BIN)
+
+#这里是诸多.d文件被包含进来，每个.d文件里都记录着一个.o文件所依赖哪些.c和.h文件。内容诸如 nginx.o: nginx.c ngx_func.h
+#有必要先判断这些文件是否存在，不然make可能会报一些.d文件找不到
+ifneq ("$(wildcard $(DEPS))","")   #如果不为空,$(wildcard)是函数【获取匹配模式文件名】，这里 用于比较是否为""
+include $(DEPS)  
+endif
+
+#----------------------------------------------------------------1begin------------------
+#$(BIN):$(OBJS)
+$(BIN):$(LINK_OBJ)
+	@echo "------------------------build $(VERSION) mode--------------------------------!!!"
+
+#一些变量：$@：目标，     $^：所有目标依赖
+# gcc -o 是生成可执行文件
+	$(CC) -o $@ $^ -lpthread
+
+#----------------------------------------------------------------1end-------------------
+
+
+#----------------------------------------------------------------2begin-----------------
+#%.o:%.c
+$(LINK_OBJ_DIR)/%.o:%.cxx
+# gcc -c是生成.o目标文件   -I可以指定头文件的路径
+#如下不排除有其他字符串，所以从其中专门把.c过滤出来 
+#$(CC) -o $@ -c $^
+	$(CC) -I$(INCLUDE_PATH) -o $@ -c $(filter %.cxx,$^)
+#----------------------------------------------------------------2end-------------------
+
+
+
+#----------------------------------------------------------------3begin-----------------
+#.d文件中的内容可能形如：nginx.o: nginx.c ngx_func.h
+#%.d:%.c
+$(DEP_DIR)/%.d:%.cxx
+#gcc -MM $^ > $@
+#gcc -MM $^ | sed 's,\(.*\)\.o[ :]*,$(LINK_OBJ_DIR)/\1.o:,g' > $@
+#echo 中 -n表示后续追加不换行
+	echo -n $(LINK_OBJ_DIR)/ > $@
+#	gcc -MM $^ | sed 's/^/$(LINK_OBJ_DIR)&/g' > $@
+#  >>表示追加
+#	gcc -I$(INCLUDE_PATH) -MM $^ >> $@
+	$(CC) -I$(INCLUDE_PATH) -MM $^ >> $@
+
+#上行处理后，.d文件中内容应该就如：/mnt/hgfs/linux/nginx/app/link_obj/nginx.o: nginx.c ngx_func.h ../signal/ngx_signal.h
+
+#----------------------------------------------------------------4begin-----------------
+
+
+
+#----------------------------------------------------------------nbegin-----------------
+#clean:			
+#rm 的-f参数是不提示强制删除
+#可能gcc会产生.gch这个优化编译速度文件
+#	rm -f $(BIN) $(OBJS) $(DEPS) *.gch
+#----------------------------------------------------------------nend------------------
+
+
+
+
+
---
+++ nginx/net/ngx_c_socket_request.cxx
@@ -0,0 +1,368 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>    //uintptr_t
+#include <stdarg.h>    //va_start....
+#include <unistd.h>    //STDERR_FILENO等
+#include <sys/time.h>  //gettimeofday
+#include <time.h>      //localtime_r
+#include <fcntl.h>     //open
+#include <errno.h>     //errno
+//#include <sys/socket.h>
+#include <sys/ioctl.h> //ioctl
+#include <arpa/inet.h>
+#include <pthread.h>   //多线程
+#include "ngx_c_conf.h"
+#include "ngx_macro.h"
+#include "ngx_global.h"
+#include "ngx_func.h"
+#include "ngx_c_socket.h"
+#include "ngx_c_memory.h"
+#include "ngx_c_lockmutex.h"  //自动释放互斥量的一个类
+
+//来数据时候的处理，当连接上有数据来的时候，本函数会被ngx_epoll_process_events()所调用  ,官方的类似函数为ngx_http_wait_request_handler();
+void CSocekt::ngx_read_request_handler(lpngx_connection_t pConn)
+{  
+    bool isflood = false; //是否flood攻击；
+
+    ssize_t reco = recvproc(pConn,pConn->precvbuf,pConn->irecvlen); 
+    if(reco <= 0)  
+    {
+        return;//该处理的上边这个recvproc()函数处理过了，这里<=0是直接return        
+    }
+
+    //走到这里，说明成功收到了一些字节（>0），就要开始判断收到了多少数据了     
+    if(pConn->curStat == _PKG_HD_INIT) //连接建立起来时肯定是这个状态，因为在ngx_get_connection()中已经把curStat成员赋值成_PKG_HD_INIT了
+    {        
+        if(reco == m_iLenPkgHeader)//正好收到完整包头，这里拆解包头
+        {   
+            ngx_wait_request_handler_proc_p1(pConn,isflood); //那就调用专门针对包头处理完整的函数去处理把。
+        }
+        else
+		{
+            pConn->curStat        = _PKG_HD_RECVING;                 //接收包头中，包头不完整，继续接收包头中	
+            pConn->precvbuf       = pConn->precvbuf + reco;              //注意收后续包的内存往后走
+            pConn->irecvlen       = pConn->irecvlen - reco;              //要收的内容当然要减少，以确保只收到完整的包头先
+        } //end  if(reco == m_iLenPkgHeader)
+    } 
+    else if(pConn->curStat == _PKG_HD_RECVING) //接收包头中，包头不完整，继续接收中，这个条件才会成立
+    {
+        if(pConn->irecvlen == reco) 
+        {
+            //包头收完整了
+            ngx_wait_request_handler_proc_p1(pConn,isflood); //那就调用专门针对包头处理完整的函数去处理把。
+        }
+        else
+		{
+			//包头还是没收完整，继续收包头
+            //pConn->curStat        = _PKG_HD_RECVING;                 //没必要
+            pConn->precvbuf       = pConn->precvbuf + reco;              //注意收后续包的内存往后走
+            pConn->irecvlen       = pConn->irecvlen - reco;              //要收的内容当然要减少，以确保只收到完整的包头先
+        }
+    }
+    else if(pConn->curStat == _PKG_BD_INIT) 
+    {
+        //包头刚好收完，准备接收包体
+        if(reco == pConn->irecvlen)
+        {
+            //收到的宽度等于要收的宽度，包体也收完整了
+            if(m_floodAkEnable == 1) 
+            {
+                //Flood攻击检测是否开启
+                isflood = TestFlood(pConn);
+            }
+            ngx_wait_request_handler_proc_plast(pConn,isflood);
+        }
+        else
+		{
+			//收到的宽度小于要收的宽度
+			pConn->curStat = _PKG_BD_RECVING;					
+			pConn->precvbuf = pConn->precvbuf + reco;
+			pConn->irecvlen = pConn->irecvlen - reco;
+		}
+    }
+    else if(pConn->curStat == _PKG_BD_RECVING) 
+    {
+        //接收包体中，包体不完整，继续接收中
+        if(pConn->irecvlen == reco)
+        {
+            //包体收完整了
+            if(m_floodAkEnable == 1) 
+            {
+                //Flood攻击检测是否开启
+                isflood = TestFlood(pConn);
+            }
+            ngx_wait_request_handler_proc_plast(pConn,isflood);
+        }
+        else
+        {
+            //包体没收完整，继续收
+            pConn->precvbuf = pConn->precvbuf + reco;
+			pConn->irecvlen = pConn->irecvlen - reco;
+        }
+    }  //end if(c->curStat == _PKG_HD_INIT)
+
+    if(isflood == true)
+    {
+        zdClosesocketProc(pConn);
+    }
+
+    return;
+}
+
+ssize_t CSocekt::recvproc(lpngx_connection_t pConn,char *buff,ssize_t buflen)  //ssize_t是有符号整型，在32位机器上等同与int，在64位机器上等同与long int，size_t就是无符号型的ssize_t
+{
+    ssize_t n;
+    
+    n = recv(pConn->fd, buff, buflen, 0); //recv()系统函数， 最后一个参数flag，一般为0；     
+    if(n == 0)
+    {
+        //ngx_log_stderr(0,"连接被客户端正常关闭[4路挥手关闭]！");
+        //ngx_close_connection(pConn);
+        //inRecyConnectQueue(pConn);
+        zdClosesocketProc(pConn);        
+        return -1;
+    }
+    //客户端没断，走这里 
+    if(n < 0) //这被认为有错误发生
+    {
+        //EAGAIN和EWOULDBLOCK[【这个应该常用在hp上】应该是一样的值，表示没收到数据，一般来讲，在ET模式下会出现这个错误，因为ET模式下是不停的recv肯定有一个时刻收到这个errno，但LT模式下一般是来事件才收，所以不该出现这个返回值
+        if(errno == EAGAIN || errno == EWOULDBLOCK)
+        {
+            ngx_log_stderr(errno,"CSocekt::recvproc()中errno == EAGAIN || errno == EWOULDBLOCK成立！");//epoll为LT模式不应该出现这个返回值，所以直接打印出来瞧瞧
+            return -1; //不当做错误处理，只是简单返回
+        }
+        //EINTR错误的产生：当阻塞于某个慢系统调用的一个进程捕获某个信号且相应信号处理函数返回时，该系统调用可能返回一个EINTR错误。
+        //例如：在socket服务器端，设置了信号捕获机制，有子进程，当在父进程阻塞于慢系统调用时由父进程捕获到了一个有效信号时，内核会致使accept返回一个EINTR错误(被中断的系统调用)。
+        if(errno == EINTR)  
+        {
+            ngx_log_stderr(errno,"CSocekt::recvproc()中errno == EINTR成立！");//epoll为LT模式不应该出现这个返回值，所以直接打印出来瞧瞧
+            return -1; //不当做错误处理，只是简单返回
+        }
+
+
+        if(errno == ECONNRESET)  //#define ECONNRESET 104 /* Connection reset by peer */
+        {
+            //如果客户端没有正常关闭socket连接，却关闭了整个运行程序【真是够粗暴无理的，应该是直接给服务器发送rst包而不是4次挥手包完成连接断开】，那么会产生这个错误            
+            //10054(WSAECONNRESET)--远程程序正在连接的时候关闭会产生这个错误--远程主机强迫关闭了一个现有的连接
+            //算常规错误吧【普通信息型】，日志都不用打印，没啥意思，太普通的错误
+            //do nothing
+
+        }
+        else
+        {
+            if(errno == EBADF)  // #define EBADF   9 /* Bad file descriptor */
+            {
+            }
+            else
+            {
+                ngx_log_stderr(errno,"CSocekt::recvproc()中发生错误，打印错误！");  //正式运营时可以考虑这些日志打印去掉
+            }
+        } 
+        
+        //ngx_log_stderr(0,"连接被客户端 非 正常关闭！");
+
+        //这种真正的错误就要，直接关闭套接字，释放连接池中连接了
+        //ngx_close_connection(pConn);
+        //inRecyConnectQueue(pConn);
+        zdClosesocketProc(pConn);
+        return -1;
+    }
+
+    return n; //返回收到的字节数
+}
+
+
+//注意参数isflood是个引用
+void CSocekt::ngx_wait_request_handler_proc_p1(lpngx_connection_t pConn,bool &isflood)
+{    
+    CMemory *p_memory = CMemory::GetInstance();		
+
+    LPCOMM_PKG_HEADER pPkgHeader;
+    pPkgHeader = (LPCOMM_PKG_HEADER)pConn->dataHeadInfo; //正好收到包头时，包头信息肯定是在dataHeadInfo里；
+
+    unsigned short e_pkgLen; 
+    e_pkgLen = ntohs(pPkgHeader->pkgLen);  //注意这里网络序转本机序，所有传输到网络上的2字节数据，都要用htons()转成网络序，所有从网络上收到的2字节数据，都要用ntohs()转成本机序
+    if(e_pkgLen < m_iLenPkgHeader)
+    {
+        //伪造包/或者包错误，否则整个包长怎么可能比包头还小？（整个包长是包头+包体，就算包体为0字节，那么至少e_pkgLen == m_iLenPkgHeader）
+        //报文总长度 < 包头长度，认定非法用户，废包
+        //状态和接收位置都复原，这些值都有必要，因为有可能在其他状态比如_PKG_HD_RECVING状态调用这个函数；
+        pConn->curStat = _PKG_HD_INIT;      
+        pConn->precvbuf = pConn->dataHeadInfo;
+        pConn->irecvlen = m_iLenPkgHeader;
+    }
+    else if(e_pkgLen > (_PKG_MAX_LENGTH-1000))   //客户端发来包居然说包长度 > 29000?肯定是恶意包
+    {
+        //恶意包，太大，认定非法用户，废包【包头中说这个包总长度这么大，这不行】
+        //状态和接收位置都复原，这些值都有必要，因为有可能在其他状态比如_PKG_HD_RECVING状态调用这个函数；
+        pConn->curStat = _PKG_HD_INIT;
+        pConn->precvbuf = pConn->dataHeadInfo;
+        pConn->irecvlen = m_iLenPkgHeader;
+    }
+    else
+    {
+        //合法的包头，继续处理
+        char *pTmpBuffer  = (char *)p_memory->AllocMemory(m_iLenMsgHeader + e_pkgLen,false); //分配内存【长度是 消息头长度  + 包头长度 + 包体长度】，最后参数先给false，表示内存不需要memset;        
+        pConn->precvMemPointer = pTmpBuffer;  //内存开始指针
+
+        //a)先填写消息头内容
+        LPSTRUC_MSG_HEADER ptmpMsgHeader = (LPSTRUC_MSG_HEADER)pTmpBuffer;
+        ptmpMsgHeader->pConn = pConn;
+        ptmpMsgHeader->iCurrsequence = pConn->iCurrsequence; //收到包时的连接池中连接序号记录到消息头里来，以备将来用；
+        //b)再填写包头内容
+        pTmpBuffer += m_iLenMsgHeader;                 //往后跳，跳过消息头，指向包头
+        memcpy(pTmpBuffer,pPkgHeader,m_iLenPkgHeader); //直接把收到的包头拷贝进来
+        if(e_pkgLen == m_iLenPkgHeader)
+        {
+            //这相当于收完整了，则直接入消息队列待后续业务逻辑线程去处理吧
+            if(m_floodAkEnable == 1) 
+            {
+                //Flood攻击检测是否开启
+                isflood = TestFlood(pConn);
+            }
+            ngx_wait_request_handler_proc_plast(pConn,isflood);
+        } 
+        else
+        {
+            pConn->curStat = _PKG_BD_INIT;                   //当前状态发生改变，包头刚好收完，准备接收包体	    
+            pConn->precvbuf = pTmpBuffer + m_iLenPkgHeader;  //pTmpBuffer指向包头，这里 + m_iLenPkgHeader后指向包体 weizhi
+            pConn->irecvlen = e_pkgLen - m_iLenPkgHeader;    //e_pkgLen是整个包【包头+包体】大小，-m_iLenPkgHeader【包头】  = 包体
+        }                       
+    }  //end if(e_pkgLen < m_iLenPkgHeader) 
+
+    return;
+}
+
+//收到一个完整包后的处理【plast表示最后阶段】，放到一个函数中，方便调用
+//注意参数isflood是个引用
+void CSocekt::ngx_wait_request_handler_proc_plast(lpngx_connection_t pConn,bool &isflood)
+{
+    //把这段内存放到消息队列中来；
+    //int irmqc = 0;  //消息队列当前信息数量
+    //inMsgRecvQueue(c->precvMemPointer,irmqc); //返回消息队列当前信息数量irmqc，是调用本函数后的消息队列中消息数量
+    //激发线程池中的某个线程来处理业务逻辑
+    //g_threadpool.Call(irmqc);
+
+    if(isflood == false)
+    {
+        g_threadpool.inMsgRecvQueueAndSignal(pConn->precvMemPointer); //入消息队列并触发线程处理消息
+    }
+    else
+    {
+        //对于有攻击倾向的恶人，先把他的包丢掉
+        CMemory *p_memory = CMemory::GetInstance();
+        p_memory->FreeMemory(pConn->precvMemPointer); //直接释放掉内存，根本不往消息队列入
+    }
+
+    pConn->precvMemPointer = NULL;
+    pConn->curStat         = _PKG_HD_INIT;     //收包状态机的状态恢复为原始态，为收下一个包做准备                    
+    pConn->precvbuf        = pConn->dataHeadInfo;  //设置好收包的位置
+    pConn->irecvlen        = m_iLenPkgHeader;  //设置好要接收数据的大小
+    return;
+}
+
+//发送数据专用函数，返回本次发送的字节数
+//返回 > 0，成功发送了一些字节
+//=0，估计对方断了
+//-1，errno == EAGAIN ，本方发送缓冲区满了
+ssize_t CSocekt::sendproc(lpngx_connection_t c,char *buff,ssize_t size)  //ssize_t是有符号整型，在32位机器上等同与int，在64位机器上等同与long int，size_t就是无符号型的ssize_t
+{
+    ssize_t   n;
+
+    for ( ;; )
+    {
+        n = send(c->fd, buff, size, 0); //send()系统函数， 最后一个参数flag，一般为0； 
+        if(n > 0) //成功发送了一些数据
+        {        
+            return n; //返回本次发送的字节数
+        }
+
+        if(n == 0)
+        {
+            return 0;
+        }
+
+        if(errno == EAGAIN)  //这东西应该等于EWOULDBLOCK
+        {
+            //内核缓冲区满，这个不算错误
+            return -1;  //表示发送缓冲区满了
+        }
+
+        if(errno == EINTR) 
+        {
+            //这个应该也不算错误 ，收到某个信号导致send产生这个错误？
+            ngx_log_stderr(errno,"CSocekt::sendproc()中send()失败.");  //打印个日志看看啥时候出这个错误
+        }
+        else
+        {
+            return -2;    
+        }
+    } //end for
+}
+
+void CSocekt::ngx_write_request_handler(lpngx_connection_t pConn)
+{      
+    CMemory *p_memory = CMemory::GetInstance();
+    
+    //这些代码的书写可以参照 void* CSocekt::ServerSendQueueThread(void* threadData)
+    ssize_t sendsize = sendproc(pConn,pConn->psendbuf,pConn->isendlen);
+
+    if(sendsize > 0 && sendsize != pConn->isendlen)
+    {        
+        //没有全部发送完毕，数据只发出去了一部分，那么发送到了哪里，剩余多少，继续记录，方便下次sendproc()时使用
+        pConn->psendbuf = pConn->psendbuf + sendsize;
+		pConn->isendlen = pConn->isendlen - sendsize;	
+        return;
+    }
+    else if(sendsize == -1)
+    {
+        ngx_log_stderr(errno,"CSocekt::ngx_write_request_handler()时if(sendsize == -1)成立，这很怪异。"); 
+        return;
+    }
+
+    if(sendsize > 0 && sendsize == pConn->isendlen) //成功发送完毕，做个通知是可以的；
+    {
+        if(ngx_epoll_oper_event(
+                pConn->fd,          //socket句柄
+                EPOLL_CTL_MOD,     
+                EPOLLOUT,           
+                1,                  //对于事件类型为增加的，EPOLL_CTL_MOD需要这个参数, 0：增加   1：去掉 2：完全覆盖
+                pConn               //连接池中的连接
+                ) == -1)
+        {
+            ngx_log_stderr(errno,"CSocekt::ngx_write_request_handler()中ngx_epoll_oper_event()失败。");
+        }    
+
+
+    }
+
+    //能走下来的，要么数据发送完毕了，要么对端断开了，那么执行收尾工作吧；
+
+    if(sem_post(&m_semEventSendQueue)==-1)
+        ngx_log_stderr(0,"CSocekt::ngx_write_request_handler()中sem_post(&m_semEventSendQueue)失败.");
+
+
+    p_memory->FreeMemory(pConn->psendMemPointer);  //释放内存
+    pConn->psendMemPointer = NULL;        
+    --pConn->iThrowsendCount;  //建议放在最后执行
+    */
+    p_memory->FreeMemory(pConn->psendMemPointer);  //释放内存
+    pConn->psendMemPointer = NULL;        
+    --pConn->iThrowsendCount;//这个值恢复了，触发下面一行的信号量才有意义
+    if(sem_post(&m_semEventSendQueue)==-1)       
+        ngx_log_stderr(0,"CSocekt::ngx_write_request_handler()中sem_post(&m_semEventSendQueue)失败.");
+
+    return;
+}
+
+//消息处理线程主函数，专门处理各种接收到的TCP消息
+//pMsgBuf：发送过来的消息缓冲区，消息本身是自解释的，通过包头可以计算整个包长
+//         消息本身格式【消息头+包头+包体】 
+void CSocekt::threadRecvProcFunc(char *pMsgBuf)
+{   
+    return;
+}
+
+
